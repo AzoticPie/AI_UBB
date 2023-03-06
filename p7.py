0be862d278fbcdc9998e5e0ec73082630aee1684
AzoticPie <39134507+AzoticPie@users.noreply.github.com>
2023-03-06 02:17:53 +0200

    Delete p7.py

--- p7.py
+++
@@ -1,9 +0,0 @@
-def k_cel_mai_mare(sir, k):
-    return sorted(sir, reverse=True)[k-1]
-
-def test_k_cel_mai_mare():
-    assert 7 == k_cel_mai_mare([7,4,6,3,9,1], 2)
-    assert 2 == k_cel_mai_mare([2,8,7,2,2,5,2,3,1], 5)
-
-test_k_cel_mai_mare()
-print("Tests successfull!")
